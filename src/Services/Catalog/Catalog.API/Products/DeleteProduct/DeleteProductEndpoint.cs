060bc70446712a1a56de417cc73667315cdde34b
Chili, Raj Kiran <rajkiran237@gmail.com>
2025-06-17 15:37:11 +0530

    CRUD operations for Product

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Catalog.API.Products.DeleteProduct;
+
+//public record DeleteProductRequest(Guid Id);//, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+public record DeleteProductResponse(bool IsSuccess);
+public class DeleteProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+        {
+            //var command = request.Adapt<DeleteProductRequest>();
+
+            var result = await sender.Send(new DeleteProductCommand(id));
+
+            var response = result.Adapt<DeleteProductResponse>();
+
+            return Results.Ok(response);
+        })
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Delete Products")
+            .WithDescription("Delete Products");
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.Extensions.Logging;
+
+namespace Catalog.API.Products.DeleteProduct;
+
+public record DeleteProductCommand(Guid Id)//, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+    :ICommand<DeleteProductResult>;
+
+public record DeleteProductResult(bool IsSuccess);
+public class DeleteProductCommandHandler
+    (IDocumentSession session, ILogger<DeleteProductCommandHandler> logger)
+    : ICommandHandler<DeleteProductCommand, DeleteProductResult>
+{
+    public async Task<DeleteProductResult> Handle(DeleteProductCommand command, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("DeleteProductCommandHandler.Handle called with {@Query}", command);
+      
+
+        session.Delete<Product>(command.Id);
+        await session.SaveChangesAsync(cancellationToken);
+
+        return new DeleteProductResult(true);
+    }
+}
